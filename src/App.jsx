e2b6d473e94ff505a6e7721e6f9f827af190c6fc
Alvin Cayo <cayoalvin991@gmail.com>
2024-06-12 11:32:14 -0400

    perfiles

--- src/App.jsx
+++ src/App.jsx
@@ -15,31 +15,31 @@ function App() {
             <img src="./src/assets/miembro1.png" alt="Miembro 1" />
             <h2>Miembro 1</h2>
             <p>ELIAN ANDRES BAPTISTA CHAVEZ</p>
-            <a href="#">Leer más</a>
+            <a href="https://www.facebook.com/anders.ludwin.7">Leer más</a>
           </div>
           <div class="member">
             <img src="./src/assets/miembro1.png" alt="Miembro 2" />
             <h2>Miembro 2</h2>
             <p>ALVIN ARIEL CAYO QUISPE</p>
-            <a href="#">Leer más</a>
+            <a href="https://www.facebook.com/alvin.cayoquispe">Leer más</a>
           </div>
           <div class="member">
             <img src="./src/assets/miembro1.png" alt="Miembro 3" />
             <h2>Miembro 3</h2>
             <p>DERIX VENEGAS NOGUERA</p>
-            <a href="#">Leer más</a>
+            <a href="https://www.facebook.com/profile.php?id=100009277001242">Leer más</a>
           </div>
           <div class="member">
             <img src="./src/assets/miembro1.png" alt="Miembro 4" />
             <h2>Miembro 4</h2>
             <p>ERIC APAZA CALLE</p>
-            <a href="#">Leer más</a>
+            <a href="https://www.facebook.com/profile.php?id=100007499419346">Leer más</a>
           </div>
           <div class="member">
             <img src="./src/assets/miembro1.png" alt="Miembro 4" />
             <h2>Miembro 4</h2>
             <p>ALVARO SANTIAGO ENCINAS FLORES</p>
-            <a href="#">Leer más</a>
+            <a href="https://www.facebook.com/alvaro.encinas.5">Leer más</a>
           </div>
         </section>
       </main>
