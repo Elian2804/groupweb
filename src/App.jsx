9a336f1e00e0f39ff07500bc8cc57d90c1a034ff
Alvin Cayo <cayoalvin991@gmail.com>
2024-06-12 11:16:21 -0400

    arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Los Sumisos</h1>
+      <main>
+        <section class="team-members">
+          <div class="member">
+            <img src="./src/assets/miembro1.png" alt="Miembro 1" />
+            <h2>Miembro 1</h2>
+            <p>ELIAN ANDRES BAPTISTA CHAVEZ</p>
+            <a href="#">Leer más</a>
+          </div>
+          <div class="member">
+            <img src="./src/assets/miembro1.png" alt="Miembro 2" />
+            <h2>Miembro 2</h2>
+            <p>ALVIN ARIEL CAYO QUISPE</p>
+            <a href="#">Leer más</a>
+          </div>
+          <div class="member">
+            <img src="./src/assets/miembro1.png" alt="Miembro 3" />
+            <h2>Miembro 3</h2>
+            <p>DERIX VENEGAS NOGUERA</p>
+            <a href="#">Leer más</a>
+          </div>
+          <div class="member">
+            <img src="./src/assets/miembro1.png" alt="Miembro 4" />
+            <h2>Miembro 4</h2>
+            <p>ERIC APAZA CALLE</p>
+            <a href="#">Leer más</a>
+          </div>
+          <div class="member">
+            <img src="./src/assets/miembro1.png" alt="Miembro 4" />
+            <h2>Miembro 4</h2>
+            <p>ALVARO SANTIAGO ENCINAS FLORES</p>
+            <a href="#">Leer más</a>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2024 Los Sumisos. Todos los derechos reservados.</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
